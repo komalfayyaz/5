160b15c45017c55e9880910c06ee7779b605c194
Fayyazkhatri <fayyazkhatri3@gmail.com>
2020-06-14 19:21:23 +0500

    now ready to commit

---
+++ app.js
@@ -0,0 +1,116 @@
+// alert("Eror ! please enter a valid password")
+
+// alert("welcome to js land \n happy coding")
+
+// alert("welcome to js land")
+// alert("happy codding! \n prevent this page from creating additional dialogs")
+
+// var name = "fayyaz"
+// console.log (name)
+
+// var   username
+// var myName = "Komal Fayyaz"
+
+// var massage
+// 3 = "hello World"
+
+// alert (message)
+
+// var name = "komal"
+// var age = "15 years old"
+// var title = "certified mobile application develpment"
+
+// alert (name )
+// alert (age)
+// alert (title)
+
+// var message 
+// message = " PIZZA \n PIZZ \n PIZ \n PI \n P"
+// alert(message)
+
+// var email
+
+// message = "my email address is "
+// email = "komalfayyaz1997@gmail.com"
+
+// // alert (message + email)
+
+// var message = "i am trying to learn from the book "
+
+// var book = " A smarter way to learn JavaScript"
+ 
+// alert(message + book)
+
+// var  message
+// message = "Yah! i can write HTML content through JavaScript"
+// document.write (message)
+
+// var message
+// message = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(message)
+
+// var age
+// age = "22"
+
+// message = "i am " 
+ 
+// message1 = " years old"
+// alert(message + age + message1)
+
+// var  message
+// var  num
+// var  message2
+  
+// message = " you have visited  this site "
+// num = " 14 "
+// message2 = " times "
+
+// alert (message + num + message2)  
+
+// var message
+// var birthyear
+// var message3
+
+// message = "My birth year is "
+// birthyear = " 1997 "
+// message3 = " Data type of my declared variable is number "
+ 
+// document.write (message + birthyear)
+// document.write (message3)
+ 
+// https://www.xyzclothing.com/
+
+// var visitorsname
+// var producttitle
+// var num 
+// var massage
+// var massage2
+
+// Visitorsname = " john doe "
+// producttitle = " T-shirts "
+// num =  5 
+// message = " orderd "
+// message2 = "(s) on XYZclothing store"
+ 
+// document.write (Visitorsname + message + num + producttitle + message2)
+   
+// var message
+// var message2
+// var message3
+// var massage4 
+
+// message = "variable names can only contain , 600, $, and _ for example :$my_1st variable" 
+// message2 = "variables must begin with a six hundred, $ or _ for example: $name,_ name or name "
+// message3 = "variable names are case sensitive"
+// message4 = "variable names should not be JS keywords"
+
+// document.write (message + message2 + message3 + message4)
+ 
+// chapter legal and illegal *****
+
+
+// cant able to pratice of java script due to some tecnical  issue in laptop.
+
+// and this is very first time i m learning javaScript so its very difficult for me to understand please
+
+// give me some to understand this.
